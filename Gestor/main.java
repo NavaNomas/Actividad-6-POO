d6beac86ec25a1c86048c915153a9c3f28b74ab9
Nava <43507363+NavaNomas@users.noreply.github.com>
2022-11-26 21:08:05 -0500

    Add files via upload

---
+++ Gestor/main.java
@@ -0,0 +1,13 @@
+
+package Gestor;
+
+public class main {
+
+    public static void main(String[] args) {
+        Formulario fm = new Formulario();
+        fm.setResizable(false);
+        fm.setLocationRelativeTo(null);
+        fm.setVisible(true);
+    }
+    
+}
